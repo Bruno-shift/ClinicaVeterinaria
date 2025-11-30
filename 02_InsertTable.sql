54c64f56a53f94e32a48935cac2225d03bb1a317
Bruno-shift <bmelo955@gmail.com>
2025-11-30 17:48:41 -0300

    Criando inserts para as tabelas

---
+++ 02_InsertTable.sql
@@ -0,0 +1,54 @@
+---Insert---
+INSERT INTO Cliente(nome, telefone)
+VALUES
+('ANA DE LIMA', '1199999-99'),
+('JOSE TESTE', '1198888-77'),
+('MARIA DO CARMO', '1195555-44');
+
+
+INSERT INTO PET(nome, especie, idade, id_cliente)
+VALUES
+('Rex', 'Cachorro',5, 3),
+('Cleyton', 'gato',4, 1),
+('Thor', 'cachorro',6, 2);
+
+
+INSERT INTO Veterinario(nome, crmv)
+VALUES
+('DRA.MARISA TESTE','SP1234'),
+('DR. BRUNO CARVALHO','SP1020'),
+('DR. TESTE','GO1234');
+
+
+INSERT INTO Consulta(data, tipo_servico, id_pet, id_veterinario)
+VALUES
+('2025-05-30','vacina',1, 1),
+('2025-06-02','castracao',2, 2),
+('2025-11-05','retorno',1, 1);
+
+
+INSERT INTO Medicamento(nome, quantidade, validade_medicamento, id_consulta)
+VALUES
+('vermit', 2, '2030-11-30',1),
+('vermit02', 3, '2030-12-30',2),
+('vermit03', 1, '2030-01-30',3);
+
+
+INSERT INTO medicamento_consulta(id_consulta, id_medicamento, dose_aplicada)
+VALUES
+(1, 1, '1 dose unica'),
+(1, 2, 'aplicar 2 gotas no ouvido'),
+(3, 2, '1 comprimido');
+
+
+select
+    co.id_consulta,
+    co.data,
+    v.nome AS veterinario,
+    v.crmv
+From consulta co
+JOIN veterinario v ON v.id_veterinario = co.id_veterinario;
+
+
+
+
